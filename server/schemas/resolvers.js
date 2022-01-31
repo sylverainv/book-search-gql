ef49925263602a267ee981ad77311fc8c4740572
sylverainv <sylverainv@gmail.com>
2022-01-31 12:04:46 -0500

    query and mutation function defined

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,7 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
+// Define the query and mutation functionality to work with the Mongoose models
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
